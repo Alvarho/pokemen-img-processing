46dd75a19cd397d151c5a7ca094568386b1c2a85
Alvarho <rhaviealvaro280199@gmail.com>
2024-04-21 16:24:05 +0700

    feat:training pipeline added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pokemonCnnClassifier import logger
 from pokemonCnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from pokemonCnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from pokemonCnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -20,6 +21,18 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   Model_trainer = ModelTrainingPipeline()
+   Model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
